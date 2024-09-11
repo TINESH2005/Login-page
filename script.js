feca7a41df2222e2fbda11cd4ebbb4581480e4c2
TINESH2005 <kalitinesh65@gmail.com>
2024-09-11 22:17:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Login button clicked!');
 });
+function togglePassword() {
+            const passwordInput = document.getElementById('password');
+            const toggleButton = document.querySelector('.toggle-password');
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                toggleButton.textContent = 'Hide';
+            } else {
+                passwordInput.type = 'password';
+                toggleButton.textContent = 'Show';
+            }
+        }
